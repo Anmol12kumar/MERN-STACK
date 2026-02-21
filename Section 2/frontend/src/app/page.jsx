5ae14ff039cdf0a1d3d4335c2f4c3cf173c9fc5a
Anmol12kumar <guptaanmolkumar37@gmail.com>
2026-02-21 13:05:39 +0530

    Add new pages for About, Contact, Login, and Signup; update Home page content

--- Section 2/frontend/src/app/page.jsx
+++ Section 2/frontend/src/app/page.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 const Home = () => {
     return (
-        <div>Home</div>
+        <div>
+            <h1 className='text-2xl bg-red-400'>Welcome to Home Page</h1>
+        </div>
     )
 }
 
